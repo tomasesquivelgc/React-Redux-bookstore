b67a0b6404619b23ffdf554f10bb793fe2ee5055
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-06-17 17:34:36 -0300

    Create Routes for Home and category

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import './App.css';
+import {
+  BrowserRouter as Router, Route, Link, Routes,
+} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+function Categories() {
+  return (
+    <div>
+      <h2>Categories</h2>
+      <p>This is the categories page.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello world</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/categories">Categories</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
