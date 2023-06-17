f0d4234ccc63b1d1c058e7049779276518ef0ed6
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-06-17 18:46:57 -0300

    Add delete function
    booklist render correct

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,30 +2,37 @@ import React from 'react';
 import BookList from './components/BookList';
 
 function Home() {
-  const books = [{
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    category: 'Action',
-    completionPercentage: '64%',
-    id: 1,
-    currentChapter: 'chapter 17',
-  },
-  {
-    title: 'Dune',
-    author: 'Franj Herbert',
-    category: 'Science fiction',
-    completionPercentage: '8%',
-    id: 2,
-    currentChapter: 'chapter 3: "A Lesson Learned"',
-  },
-  {
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    category: 'Economy',
-    completionPercentage: '0%',
-    id: 3,
-    currentChapter: 'Introduction',
-  }];
+  const [books, setBooks] = React.useState([
+    {
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      category: 'Action',
+      completionPercentage: '64',
+      id: 1,
+      currentChapter: 'chapter 17',
+    },
+    {
+      title: 'Dune',
+      author: 'Franj Herbert',
+      category: 'Science fiction',
+      completionPercentage: '8',
+      id: 2,
+      currentChapter: 'chapter 3: "A Lesson Learned"',
+    },
+    {
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+      category: 'Economy',
+      completionPercentage: '0',
+      id: 3,
+      currentChapter: 'Introduction',
+    },
+  ]);
+
+  const handleDeleteBook = (bookId) => {
+    setBooks((prevBooks) => prevBooks.filter((book) => book.id !== bookId));
+  };
+
   return (
     <div>
       <h2>My book list</h2>
