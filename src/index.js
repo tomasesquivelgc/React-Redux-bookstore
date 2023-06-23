a57d7cd48c8a0d39c41bfb0c553b338337943f3e
TomasEsquivel <117373513+tomasesquivelgc@users.noreply.github.com>
2023-06-22 22:05:17 -0300

    SetUp initial files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import store from './app/store';
 import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 );
